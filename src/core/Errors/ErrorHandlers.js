9fa82d9ec8c5837ef9e0505fef6ff652df426d9e
Ali Wahab <alimemonsd@gmail.com>
2021-04-02 21:59:36 +0500

    Added more error

--- src/core/Errors/ErrorHandlers.js
+++ src/core/Errors/ErrorHandlers.js
@@ -18,6 +18,26 @@ export default class ErrorHandlers {
       return "Can not load video due to internet connection";
     } else if (this.detail == Hls.ErrorDetails.FRAG_LOAD_ERROR) {
       return "Interrupted internet connection. Can not download video data.";
+    } else if (this.detail == Hls.ErrorDetails.FRAG_LOAD_TIMEOUT) {
+      return "Failed to load video data, because of network connection.";
+    } else if (this.detail == Hls.ErrorDetails.KEY_LOAD_TIMEOUT) {
+      return "Failed to load video data, because of network connection.";
+    } else if (
+      this.detail == Hls.ErrorDetails.MANIFEST_INCOMPATIBLE_CODECS_ERROR
+    ) {
+      return "The media file doesn't contain content";
+    } else if (this.detail == Hls.ErrorDetails.FRAG_DECRYPT_ERROR) {
+      return "Can not decrypt video data. The video is corrupted";
+    } else if (this.detail == Hls.ErrorDetails.FRAG_PARSING_ERROR) {
+      return "Can not parse video data. The video is corrupted";
+    } else if (
+      this.detail == Hls.ErrorDetails.BUFFER_INCOMPATIBLE_CODECS_ERROR
+    ) {
+      return "Incompatible video codec";
+    } else if (this.detail == Hls.ErrorDetails.BUFFER_NUDGE_ON_STALL) {
+      return "Can not load video data";
+    } else if (this.detail == Hls.ErrorDetails.INTERNAL_EXCEPTION) {
+      return "Oops something crashed...";
     } else {
       return "Unknown Error";
     }
